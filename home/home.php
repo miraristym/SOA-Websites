0ab3032cd7ecd52884048478787c3561d5e4fddd
miraristym <44823779+miraristym@users.noreply.github.com>
2019-01-21 19:58:51 +0700

    Add files via upload

---
+++ home/home.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.standalone.min.css">
+  <link rel="stylesheet" href="now-ui-kit.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/nucleo-icons.css" type="text/css">
+  <script src="assets/js/navbar-ontop.js"></script>
+  <link rel="icon" href="https://templates.pingendo.com/assets/Pingendo_favicon.ico">
+  <title>International Relation Office</title>
+  <meta name="description" content="Start your development with a beautiful Bootstrap 4 UI kit. It is yours Free.">
+  <meta name="keywords" content="bootstrap 4, bootstrap 4 uit kit, bootstrap 4 kit, now ui, now ui kit, creative tim, html kit, html css template, web template, bootstrap, bootstrap 4, css3 template, frontend, responsive bootstrap template, bootstrap ui kit, responsive ui kit">
+</head>
+
+<body class="">
+  <nav class="navbar bg-primary navbar-dark">
+    <div class="container"> <a class="navbar-brand" href="#" contenteditable="true">International Relation Office</a> </div>
+  </nav>
+  <div class="py-5">
+    <div class="image">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="carousel-item active"> <img class="d-block mx-auto" src="assets/img/globe.png">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5" style="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="text-center text-secondary">(International Relation Office) IRO's Services</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3"><img class="img-fluid d-block img-thumbnail" src="assets/img/news.png">
+          <div class="row">
+            <div class="col-md-12"><button class="btn btn-block btn-outline-dark text-dark" onclick="window.location='../news/news_default.html'">NEWS</button></div>
+          </div>
+        </div>
+        <div class="col-md-3"><img class="img-fluid d-block img-thumnail my-3 mx-auto" src="assets/img/register.png" style="">
+          <div class="row">
+            <div class="col-md-12"><a class="btn btn-block btn-outline-dark text-dark" href="../enrollment/enrollment.php">ENROLLMENT</a></div>
+          </div>
+        </div>
+        <div class="col-md-3"><img class="img-fluid d-block img-thumbnail my-2 mx-auto p-2" src="assets/img/reenrollment.png">
+          <div class="row">
+            <div class="col-md-12"><a class="btn btn-block btn-outline-dark text-dark" href="../reenrollment/reenrollment.php">RE-ENROLLMENT</a></div>
+          </div>
+        </div>
+        <div class="col-md-3"><img class="img-fluid d-block rounded-circle mx-auto p-3" src="assets/img/consulting.png">
+          <div class="row">
+            <div class="col-md-12"><a class="btn btn-block btn-outline-dark text-dark" onclick="window.location='../consulting/index.php'">CONSULTING</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="mx-auto col-xl-8 col-md-10">
+          <h2 class="my-3 text-center">
+            <b>Welcome to IRO</b></h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-dark" style="background-image: url(&quot;assets/img/bg11.jpg&quot;); background-size: cover; background-position: center top;">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-success">
+          <div class="row">
+            <p class="lead text-dark text-justify">The International Relation Office (IRO) is an institute under the Directorate of Partnership and International Relations – ITB – which provides professional supports and advising services to international students who are coming for full-time study and exchange programs, and scholars regarding their study and immigration status in Indonesia. This office is also committed to providing quality advising services and supportive programming to ITB students who are participating in exchange programs.<br><br></p>
+          </div>
+        </div>
+        <p class="lead text-justify text-light">Our main objective is to help the international students, scholars, and ITB faculty and staff, navigate the many complexities associated with student-and employment-based visa arrangement. We are commited to providing information and services, taking into account institutional policies and ever-changing state regulations.&nbsp;<br><br></p>
+      </div>
+      <div class="row text-center">
+        <p class="lead text-justify">It is our pleasure to welcome you to the IRO – ITB. Whether you are a perspective student, a student already enrolled in a study program, a faculty contact, or a member of the greater – ITB network. We’d also like to encourage you to contact us with any questions you might have regarding the services we provide and information contained on our site.&nbsp;<br><br>Dr. Edwan Kardena&nbsp;<br>Director The Directorate of Partnership and International Relations&nbsp;<br>Institut Teknologi Bandung</p>
+      </div>
+    </div>
+  </div>
+  <div class="pt-5 bg-dark">
+    <div class="container">
+      <div class="row mt-4 py-5">
+        <div class="col-md-12 text-center">
+          <h3 class="mb-5 text-center">Thank you for supporting us!</h3>
+          <button class="btn btn-icon rounded btn-lg btn-light mx-1 btn-twitter" type="button" data-placement="top" data-toggle="tooltip" title="Follow us">
+            <i class="fa fa-fw fa-2x fa-twitter"></i>
+          </button>
+          <button class="btn btn-icon rounded btn-lg btn-light mx-1 btn-facebook" type="button" data-placement="top" data-toggle="tooltip" title="Like us">
+            <i class="fa fa-fw fa-2x fa-facebook-square"></i>
+          </button>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div> </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="assets/js/parallax.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('[data-toggle="popover"]').popover();
+      $('[data-toggle="tooltip"]').tooltip();
+      $('#datepicker-example').datepicker({
+        calendarWeeks: true,
+        autoclose: true,
+        todayHighlight: true
+      });
+    });
+  </script>
+</body>
+
+</html>
